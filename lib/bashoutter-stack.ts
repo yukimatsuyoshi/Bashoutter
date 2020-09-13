3603deace7f6bdafa717f0ea81c1abedbe0fd0cb
Yuki Matsuyoshi <sento.mikaruge@gmail.com>
2020-09-13 23:55:40 +0900

    コード自体は書けた

--- lib/bashoutter-stack.ts
+++ lib/bashoutter-stack.ts
@@ -26,7 +26,7 @@ export class BashoutterStack extends cdk.Stack {
     })
     new s3_deploy.BucketDeployment(this, "BucketDeployment", {
       destinationBucket: bucket,
-      sources: [s3_deploy.Source.asset("./gui/dist")],
+      sources: [s3_deploy.Source.asset("../gui/dist")],
       retainOnDelete: false
     })
 
@@ -44,6 +44,69 @@ export class BashoutterStack extends cdk.Stack {
       memorySize: 512,
       timeout: cdk.Duration.seconds(10),
       environment: common_params.environment
+    })
+    const post_haiku_lambda = new _lambda.Function(this, "PostHaiku", {
+      runtime: common_params.runtime,
+      code: _lambda.Code.fromAsset("api"),
+      handler: "api.post_haiku",
+      environment: common_params.environment
+    })
+    const patch_haiku_lambda = new _lambda.Function(this, "PatchHaiku", {
+      runtime: common_params.runtime,
+      code: _lambda.Code.fromAsset("api"),
+      handler: "api.patch_haiku",
+      environment: common_params.environment
+    })
+    const delete_haiku_lambda = new _lambda.Function(this, "DeleteHaiku", {
+      runtime: common_params.runtime,
+      code: _lambda.Code.fromAsset("api"),
+      handler: "api.delete_haiku",
+      environment: common_params.environment
+    })
+
+    table.grantReadData(get_haiku_lambda)
+    table.grantReadWriteData(post_haiku_lambda)
+    table.grantReadWriteData(patch_haiku_lambda)
+    table.grantReadWriteData(delete_haiku_lambda)
+
+    const api = new apigw.RestApi(this, "BashoutterApi", {
+      defaultCorsPreflightOptions:  {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS
+      }
+    })
+
+    const haiku = api.root.addResource("haiku")
+    haiku.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(get_haiku_lambda)
+    )
+    haiku.addMethod(
+      "POST",
+      new apigw.LambdaIntegration(post_haiku_lambda)
+    )
+
+    const haiku_item_id = haiku.addResource("{item_id}")
+    haiku_item_id.addMethod(
+      "PATCH",
+      new apigw.LambdaIntegration(patch_haiku_lambda)
+    )
+    haiku_item_id.addMethod(
+      "DELETE",
+      new apigw.LambdaIntegration(delete_haiku_lambda)
+    )
+
+    new ssm.StringParameter(this, "TABLE_NAME", {
+      parameterName: "TABLE_NAME",
+      stringValue: table.tableName
+    })
+    new ssm.StringParameter(this, "ENDPOINT_URL", {
+      parameterName: "ENDPOINT_URL",
+      stringValue: api.url
+    })
+
+    new cdk.CfnOutput(this, "BucketUrl", {
+      value: bucket.bucketWebsiteDomainName
     })
   }
 }
